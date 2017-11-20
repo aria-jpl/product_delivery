8218e507b6c3423f165a796360b5d813b82d9a05
Malarout <namrata.malarout@jpl.nasa.gov>
2017-11-20 10:17:18 -0800

    malarout: added code to put object

--- merge_metadata.py
+++ merge_metadata.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 import os, sys, json, logging, traceback
-
+import boto3
 
 log_format = "[%(asctime)s: %(levelname)s/%(funcName)s] %(message)s"
 logging.basicConfig(format=log_format, level=logging.INFO)
@@ -23,9 +23,18 @@ def merge(met_file, ds_file, merged_file):
     logger.info("merged: {}".format(json.dumps(ds, indent=2)))
     logger.info("written to: {}".format(merged_file))
 
+def putFile(merged_file, bucket_name, key_name):
+    """Put merged metadata file on s3 bucket"""
+    s3 = boto3.client('s3')
+    s3.upload_file(merged_file, bucket_name, key_name)
 
 if __name__ == '__main__':
     try: status = merge(sys.argv[1], sys.argv[2], sys.argv[3])
+         key = sys.argv[3]
+         datasets_pos = key.find("/datasets")
+         bucket_pos = key.rfind('/', 0, datasets_pos)
+         bucket_name = key[bucket_pos+1:datasets_pos]
+         #putFile(sys.argv[3], bucket_name, key)
     except Exception as e:
         with open('_alt_error.txt', 'w') as f:
             f.write("%s\n" % str(e))
