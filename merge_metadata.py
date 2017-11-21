83e756cc83c9e60b12ab3db17b05fe9e349235c9
Malarout <namrata.malarout@jpl.nasa.gov>
2017-11-21 08:49:34 -0800

    malarout: update to py script to replace quotes in params

--- merge_metadata.py
+++ merge_metadata.py
@@ -9,6 +9,12 @@ logger = logging.getLogger('merge_metadata')
 
 def merge(met_file, ds_file, merged_file):
     """Merge met and dataset JSON file."""
+    if met_file.startswith('"'):
+        met_file = met_file[1:-1]
+    if ds_file.startswith('"'):
+        ds_file = ds_file[1:-1]
+    if merged_file.startswith('"'):
+        merged_file = merged_file[1:-1]
 
     with open(ds_file) as f:
         ds = json.load(f)
@@ -30,13 +36,30 @@ def putFile(merged_file, bucket_name, key_name):
     s3.upload_file(merged_file, bucket_name, key_name)
 
 if __name__ == '__main__':
-    try: 
-        status = merge(sys.argv[1], sys.argv[2], sys.argv[3])
+    try:
+        met_file = sys.argv[1]
+        ds_file = sys.argv[2]
+        merged_file = sys.argv[3]
+
+        if met_file.startswith('"'):
+            met_file = met_file.replace('"','')
+
+        if ds_file.startswith('"'):
+            ds_file = ds_file.replace('"','')
+
+        if merged_file.startswith('"'):
+            merged_file = merged_file.replace('"','')
+
+        status = merge(met_file, ds_file, merged_file)
+
         key = sys.argv[4]
+        if key.startswith('"'):
+            key = key..replace('"','')
+ 
         datasets_pos = key.find("/datasets")
         bucket_pos = key.rfind('/', 0, datasets_pos)
         bucket_name = key[bucket_pos+1:datasets_pos]
-        putFile(sys.argv[3], bucket_name, key)
+        putFile(merged_file, bucket_name, key)
     except Exception as e:
         with open('_alt_error.txt', 'w') as f:
             f.write("%s\n" % str(e))
